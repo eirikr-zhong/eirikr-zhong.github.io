9ff70cf175dd91b8c3459a3ec92c280b15d0d447
Eirikr Zhong <127606074+eirikr-zhong@users.noreply.github.com>
2023-03-19 22:36:28 +0800

    Revert "try fix"
    This reverts commit 6ebda395bdcb3f9bb3f42732f65318b0604c21e7.

--- themes/manis/static/js/dark-mode.js
+++ themes/manis/static/js/dark-mode.js
@@ -12,9 +12,9 @@ var savedTheme = localStorage.getItem("dark-mode-storage") || systemDefault;
 setTheme(savedTheme);
 
 toggle.addEventListener("click", () => {
-    if (toggle.src.endsWith("../img/moon.svg") ) {
+    if (toggle.src.endsWith("/img/moon.svg") ) {
         setTheme("dark");
-    } else if (toggle.src.endsWith("../img/sun.svg") ) {
+    } else if (toggle.src.endsWith("/img/sun.svg") ) {
         setTheme("light");
     }
 
@@ -25,9 +25,9 @@ function setTheme(mode) {
 
     if (mode === "dark") {
         darkTheme.disabled = false;
-        toggle.src = "../img/sun.svg";
+        toggle.src = "/img/sun.svg";
     } else if (mode === "light") {
         darkTheme.disabled = true;
-        toggle.src = "../img/moon.svg";
+        toggle.src = "/img/moon.svg";
     }
 }
